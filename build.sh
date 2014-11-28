2916c8cb830f1a9d62d036ce41fdcc3ae2057d4e
rubicks <rubicks00@gmail.com>
2014-11-28 13:22:44 -0500

    makes build.sh fail when ghc missing

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+#
+# rubicks-blog/build.sh
 
-echo -n && \
-    ghc --make site.hs && \
-    ./site build && \
-    echo "big win" || echo "oh noes"
+_this=$(readlink -f ${BASH_SOURCE})
+_here=$(dirname ${_this})
+
+cd ${_here}        || exit 1
+which ghc          || exit 1
+ghc --version      || exit 1
+ghc --make site.hs || exit 1
+./site build       || exit 1
