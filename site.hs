b698677429d707fe619bbcec9dddee769c06d689
rubicks <rubicks00@gmail.com>
2014-12-04 11:44:52 -0500

    changes date format -- year goes first

--- site.hs
+++ site.hs
@@ -67,5 +67,5 @@ main = hakyll $ do
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
+    dateField "date" "%Y %B %e" `mappend`
     defaultContext
