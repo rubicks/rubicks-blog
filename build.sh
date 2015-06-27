4ce78c1cba09dfa2cdfea7282379911fc60f9cb8
rubicks <naroza@gmail.com>
2015-06-27 15:31:06 -0400

    cleans build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 #
-# rubicks-blog/build.sh
+# github.com/rubicks/rubicks-blog/build.sh
 
-_this=$(readlink -f ${BASH_SOURCE})
-_here=$(dirname ${_this})
+trap 'exit 1' ERR
+set -e
 
-echo -n                   && \
-    cd ${_here}           && \
-    which ghc             && \
-    ghc --version         && \
-    ghc --make site.hs    && \
-    ${_here}/site rebuild
+_here=$(dirname $(readlink -f ${BASH_SOURCE}))
+
+cd ${_here}
+which ghc
+ghc --version
+ghc --make site.hs
+${_here}/site rebuild
 
 # # maybe someday travis will update its linkchecker, this one throws too many errors
 # find ${_here}/_site -name "*.html" | xargs linkchecker --no-warnings
